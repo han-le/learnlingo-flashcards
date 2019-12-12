51e967d02713b104214df41680e01db670fe8da6
han-le <58657916+han-le@users.noreply.github.com>
2019-12-12 21:23:00 +0700

    Adjust row padding

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -7,10 +7,10 @@
     <link href="https://fonts.googleapis.com/css?family=Neucha|Raleway:900&display=swap" rel="stylesheet">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="/css/style.css">
 
     <title>Learn Lingo - English Flash Cards</title>
 </head>
@@ -51,7 +51,7 @@
 <!-- Body -->
 
 <div class="container">
-    <div class="row">
+    <div class="row no-gutters">
         <!-- PIC 1 -->
         @foreach($post_data as $post)
 
